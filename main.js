25d295c857d784515b938f5a8330725ea3bcb7fe
mxw74 <marcus.walls@utexas.edu>
2022-06-13 11:58:05 -0500

    my first calculator

--- main.js
+++ main.js
@@ -31,16 +31,24 @@ const subtract = (numA, numB) => {
 
 const multiply = (numA, numB) => {
   // * to get a product then return it
+  const product = numA * numB
   // Open up the inspector tool in Chrome and select the Console tab to see what this functions is "logging out" to the console.
   console.log(numA, numB)
+  return product
 }
 
-const divide = null
+const divide = (numA, numB) => {
+const quotient = numA / numB
+console.log (numA, numB)
+return quotient
 // / to get a quotient,
-
-const modulus = null
+}
+const modulus = (numA, numB) => {
+  const remainder = numA % numB
+  console.log (numA, numB)
+  return remainder
 // and % to get a remainder.
-
+}
 // This function changes the "operation" variable to be equal to the "id" of the button we choose on the web page.
 const changeOperation = (chosenOperation) => {
   operation = chosenOperation
@@ -64,11 +72,11 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(multiply(firstNum, secondNum)) 
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElement(divide(firstNum, secondNum))
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElement(modulus(firstNum, secondNum))
     break;
     default: "Choose an operation"
   }
